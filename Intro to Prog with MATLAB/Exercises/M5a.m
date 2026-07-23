21374475fbe48e3b5a54e6d8b67946086209ec50
Ayobami Faith Jr. <50885913+AyFait@users.noreply.github.com>
2026-07-23 11:06:14 +0100

    Update M5a.m

--- Intro to Prog with MATLAB/Exercises/M5a.m
+++ Intro to Prog with MATLAB/Exercises/M5a.m
@@ -1,3 +1,10 @@
+%{
+Write a function called minimax that takes M, a matrix input argument and returns mmr, 
+a row vector containing the absolute values of the difference between the maximum and minimum valued elements in each row. As a second output argument called mmm,
+it provides the difference between the maximum and minimum element in the entire matrix.
+%}
+
+
 function [mmr, mmm] = minimax(M)
 M %get a view of M
 [m,n] = size(M) %the rxc of M
